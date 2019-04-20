c2ba2feae3f92e905aefe2c23ef2b5826c4b2446
Cryptopurr Columbu <cryptopurrcolumbus@gmail.com>
2019-04-20 16:58:31 +0530

    Add mapbox api key, add css for filter options
    Signed-off-by: Cryptopurr Columbu <cryptopurrcolumbus@gmail.com>

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiY3J5cHRvcHVycmNvbHVtYnVzIiwiYSI6ImNqdjg1cG4yYjBuNm80OXBmbHF6cXFlMnYifQ.rZU5VXPPWUxLTNGg7kTg8w',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -34,7 +34,6 @@ initMap = () => {
     }
   });
 }  
- 
 /* window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
